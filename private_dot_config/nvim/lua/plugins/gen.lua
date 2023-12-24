d7964dfda5b27d516041e467372359be3d372cef
Alankrit <alankritjoshi@gmail.com>
2023-12-23 23:54:01 -0800

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/gen.lua

--- private_dot_config/nvim/lua/plugins/gen.lua
+++ private_dot_config/nvim/lua/plugins/gen.lua
@@ -1,10 +1,32 @@
 return {
-  "David-Kunz/gen.nvim",
-  commands = { "Gen" },
-  keys = {
-    { "<leader>cg", ":Gen<CR>", desc = "Ollama Generate", mode = { "n", "v" } },
+  "nomnivore/ollama.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
   },
-  config = function()
-    require("gen").model = "codellama"
-  end,
+
+  -- All the user commands added by the plugin
+  cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+
+  keys = {
+    -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+    {
+      "<leader>oo",
+      ":<c-u>lua require('ollama').prompt()<cr>",
+      desc = "ollama prompt",
+      mode = { "n", "v" },
+    },
+
+    -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+    {
+      "<leader>oG",
+      ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+      desc = "ollama Generate Code",
+      mode = { "n", "v" },
+    },
+  },
+
+  ---@type Ollama.Config
+  opts = {
+    model = "dolphin-mixtral:latest",
+  },
 }
