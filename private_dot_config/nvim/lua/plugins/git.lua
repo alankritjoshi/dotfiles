4c4f18a7044c6cc43dc2715be2f98e56b58e4a56
Alankrit Joshi <alankritjoshi@gmail.com>
2024-03-11 15:04:56 -0700

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/git.lua

--- private_dot_config/nvim/lua/plugins/git.lua
+++ private_dot_config/nvim/lua/plugins/git.lua
@@ -1,3 +1,13 @@
 return {
-  "tpope/vim-fugitive",
+  { "tpope/vim-fugitive" },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        -- delay = 0,
+      },
+    },
+  },
 }
