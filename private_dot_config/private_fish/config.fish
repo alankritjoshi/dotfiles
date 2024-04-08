707167306a70ae2ec8aecb3970bc4fc1588c7fe0
Alankrit Joshi <alankritjoshi@gmail.com>
2024-04-07 23:40:43 -0700

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -144,5 +144,4 @@ starship init fish | source
 if status is-interactive
     # Commands to run in interactive sessions can go here
     # eval (zellij setup --generate-auto-start fish | string collect)
-    cd $HOME/dev
 end
