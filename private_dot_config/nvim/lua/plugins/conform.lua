3fed9174e17bd9c375d5c0eb58c13ef16fe74ead
Alankrit Joshi <alankritjoshi@gmail.com>
2024-05-17 00:03:00 -0700

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/conform.lua Update .config/skhd/skhdrc Update .config/yabai/yabairc

--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -3,6 +3,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        json = { "prettier" },
         javascript = { "prettier" },
         typescript = { "prettier" },
         html = { "rustywind", "htmlbeautifier" },
