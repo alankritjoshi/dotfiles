3fa0499a40d60c6d680b70d2d02c7f662c698683
Alankrit Joshi <alankritjoshi@gmail.com>
2024-06-02 00:38:48 -0700

    Add .config/nvim/lua/plugins/mini-pairs.lua Add .config/nvim/lua/plugins/persisted.lua

---
+++ private_dot_config/nvim/lua/plugins/persisted.lua
@@ -0,0 +1,50 @@
+return {
+  "olimorris/persisted.nvim",
+  lazy = false,
+  keys = {
+    { "<leader>qf", "<cmd>Telescope persisted<cr>", desc = "Search the current sessions" },
+    { "<leader>qt", "<cmd>SessionToggle<cr>", desc = "Determines whether to load, start or stop a session" },
+    { "<leader>qs", "<cmd>SessionSave<cr>", desc = "Save the current session" },
+    { "<leader>qd", "<cmd>SessionDelete<cr>", desc = "Delete the current session" },
+  },
+  config = function()
+    require("persisted").setup({
+      autosave = true, -- automatically save session files when exiting Neovim
+      should_autosave = function()
+        local excluded_filetypes = {
+          "alpha",
+          "oil",
+          "lazy",
+          "",
+        }
+
+        for _, filetype in ipairs(excluded_filetypes) do
+          if vim.bo.filetype == filetype then
+            return false
+          end
+        end
+
+        return true
+      end,
+      autoload = true, -- automatically load the session for the cwd on Neovim startup
+      on_autoload_no_session = function()
+        vim.notify("No existing session to load.")
+      end,
+      use_git_branch = false,
+      ignored_dirs = {
+        { "~/", exact = true },
+        { "/", exact = true },
+        { "/tmp", exact = true },
+        { "/dev", exact = true },
+        { "/Downloads", exact = true },
+        { "/Documents", exact = true },
+      },
+      telescope = {
+        mappings = { -- table of mappings for the Telescope extension
+          copy_session = "<C-s>",
+          delete_session = "<c-x>",
+        },
+      },
+    })
+  end,
+}
