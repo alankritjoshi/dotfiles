b67279f19d34401d02366e206859d300c762106e
Alankrit Joshi <alankritjoshi@gmail.com>
2024-06-03 16:09:53 -0700

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -144,7 +144,7 @@ end
 
 function _find_and_edit_dir_with_zellij_pane
     while true
-        set -l ZJ_DIR_PATH (zoxide query --interactive)
+        set -l ZJ_DIR_PATH (zoxide query --list | fzf)
         if test $status -ne 0
             break
         else
@@ -155,7 +155,7 @@ end
 
 function _find_and_edit_dir_with_zellij_tab
     while true
-        set -l ZJ_DIR_PATH (zoxide query --interactive)
+        set -l ZJ_DIR_PATH (zoxide query --interactive | fzf)
         if test $status -ne 0
             break
         else
