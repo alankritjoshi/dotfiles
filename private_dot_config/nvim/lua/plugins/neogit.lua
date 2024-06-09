6f08df748c44af1754871711d210b788079a4c30
Alankrit Joshi <alankritjoshi@gmail.com>
2024-06-09 00:10:44 -0700

    Add .config/nvim/after/ftplugin/go.lua Add .config/nvim/after/ftplugin/sh.lua Add .config/nvim/lua/plugins/mini.lua Add .config/nvim/lua/plugins/neogit.lua

---
+++ private_dot_config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    keys = {
+      {
+        "<leader>gt",
+        "<leader><cmd>Neogit kind=split_above<CR>",
+        desc = "Neogit",
+      },
+    },
+  },
+}
