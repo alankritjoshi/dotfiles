753417b611ab5bd8964e3afb3b32976f1ab960cc
Alankrit Joshi <alankritjoshi@gmail.com>
2024-06-15 01:11:58 -0700

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Remove .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/autocmds.lua Update .config/nvim/lua/plugins/git.lua Update .config/nvim/lua/plugins/yanky.lua

--- private_dot_config/nvim/lua/plugins/git.lua
+++ private_dot_config/nvim/lua/plugins/git.lua
@@ -1,5 +1,4 @@
 return {
-  { "tpope/vim-fugitive" },
   {
     "lewis6991/gitsigns.nvim",
     opts = {
@@ -13,10 +12,9 @@ return {
   {
     "NeogitOrg/neogit",
     dependencies = {
-      "nvim-lua/plenary.nvim", -- required
-      "sindrets/diffview.nvim", -- optional - Diff integration
-
-      "nvim-telescope/telescope.nvim", -- optional
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "ibhagwan/fzf-lua",
     },
     config = true,
   },
