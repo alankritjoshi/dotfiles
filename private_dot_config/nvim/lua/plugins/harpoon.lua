45dbfc1806b636e4e89db1d5b166b0f478c86b74
Alankrit Joshi <alankritjoshi@gmail.com>
2024-06-16 23:16:01 -0700

    Update .config/nvim/lua/plugins/harpoon.lua

--- private_dot_config/nvim/lua/plugins/harpoon.lua
+++ private_dot_config/nvim/lua/plugins/harpoon.lua
@@ -27,6 +27,24 @@ return {
           end,
           desc = "Harpoon Quick Menu",
         },
+        {
+          "<leader>0",
+          function()
+            local harpoon = require("harpoon")
+            local marks = harpoon:list().items
+
+            for i, _ in ipairs(marks) do
+              require("harpoon"):list():select(i)
+            end
+
+            if #marks == 0 then
+              vim.notify("No Harpoon marks!")
+            else
+              require("harpoon"):list():select(1)
+            end
+          end,
+          desc = "Harpoon open all",
+        },
       }
 
       for i = 1, 5 do
