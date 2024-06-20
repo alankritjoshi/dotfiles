503e14886f8631f35fc800ec09b48fbdf8057c04
Alankrit Joshi <alankritjoshi@gmail.com>
2024-06-19 22:45:56 -0700

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/git.lua

--- private_dot_config/nvim/lua/plugins/git.lua
+++ private_dot_config/nvim/lua/plugins/git.lua
@@ -18,4 +18,13 @@ return {
     },
     config = true,
   },
+  {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    opts = {},
+    keys = {
+      { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+      { "<leader>gw", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+    },
+  },
 }
