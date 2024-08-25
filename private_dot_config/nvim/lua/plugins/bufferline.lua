c90c331be1d5b4e3e75892181cb5972d48f4c512
Alankrit Joshi <alankritjoshi@gmail.com>
2024-08-25 00:46:03 -0700

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/bufferline.lua Update .config/nvim/lua/plugins/fzf.lua

--- private_dot_config/nvim/lua/plugins/bufferline.lua
+++ private_dot_config/nvim/lua/plugins/bufferline.lua
@@ -15,67 +15,6 @@ return {
         "<Cmd>BufferLineCloseOthers<CR><Cmd>bdelete<CR>",
         desc = "Delete Other Buffers",
       },
-      {
-        "<leader>bh",
-        function()
-          -- Select last Harpoon buffer
-          local harpoon = require("harpoon")
-          local marks = harpoon:list().items
-          local l = #marks
-          harpoon:list():select(l)
-
-          -- Delete all buffers to the right of the last Harpoon buffer
-          vim.cmd("BufferLineCloseRight")
-        end,
-        desc = "Delete Non-Harpoon Buffers",
-      },
-    },
-    opts = {
-      offsets = nil,
-      options = {
-        -- Show numbers only for Harpoon marks
-        numbers = function(opts)
-          local harpoon = require("harpoon")
-          local marks = harpoon:list().items
-
-          local bufname = vim.fn.bufname(opts.id)
-
-          for i, mark in ipairs(marks) do
-            if bufname == mark.value then
-              return i
-            end
-          end
-
-          return ""
-        end,
-
-        -- Always put sorted Harpoon marks to the beginning of all buffers
-        sort_by = function(buffer_a, buffer_b)
-          local harpoon = require("harpoon")
-          local marks = harpoon:list().items
-
-          -- Use large values for non-Harpoon buffers to show them last
-          local a = 1000
-          local b = 1000
-
-          local buffer_a_name = vim.fn.bufname(buffer_a.id)
-          local buffer_b_name = vim.fn.bufname(buffer_b.id)
-
-          -- Use index of marks for sorting Harpoon buffers
-          for index, mark in ipairs(marks) do
-            if buffer_a_name == mark.value then
-              a = index
-              break
-            end
-            if buffer_b_name == mark.value then
-              b = index
-              break
-            end
-          end
-
-          return a < b
-        end,
-      },
     },
   },
 }
--- private_dot_config/nvim/lua/plugins/fzf.lua
+++ private_dot_config/nvim/lua/plugins/fzf.lua
@@ -2,42 +2,81 @@ return {
   {
     "ibhagwan/fzf-lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    -- config = function()
-    --   local fzf_lua = function()
-    --     return require("fzf-lua")
-    --   end
-    --
-    --   fzf_lua().setup({
-    --     files = {
-    --       fd_opts = [[--color=never --type file --type dir --hidden --follow --exclude .git]],
-    --       actions = {
-    --         ["ctrl-e"] = {
-    --           -- filter by selected subdirectory
-    --           function(selected, opts)
-    --             local entry = fzf_lua().path.entry_to_file(selected[1])
-    --             if not fzf_lua().path.is_absolute(entry.path) then
-    --               entry.path = fzf_lua().path.join({ opts.cwd or vim.uv.cwd(), entry.path })
-    --             end
-    --
-    --             -- if not directory, just refresh
-    --             if not entry.path or vim.fn.isdirectory(entry.path) ~= 1 then
-    --               entry.path = opts.cwd
-    --             end
-    --
-    --             fzf_lua().files({ cwd = entry.path })
-    --           end,
-    --         },
-    --         ["ctrl-r"] = {
-    --           -- expand filter to parent directory
-    --           function(_, opts)
-    --             fzf_lua().files({
-    --               cwd = vim.fn.fnamemodify(vim.fs.normalize(opts.cwd or vim.uv.cwd()), ":h"),
-    --             })
-    --           end,
-    --         },
-    --       },
-    --     },
-    --   })
-    -- end,
+    keys = {
+      {
+        "<leader>fd",
+        function()
+          require("fzf-lua").files({
+            cwd = vim.fn.getcwd(),
+            fd_opts = "--type d",
+            prompt = "Directories❯ ",
+          })
+        end,
+        desc = "Search Directories in CWD",
+      },
+    },
+    config = function()
+      local fzf_lua = function()
+        return require("fzf-lua")
+      end
+
+      fzf_lua().setup({
+        winopts = {
+          preview = {
+            layout = "vertical",
+            vertical = "down:60%",
+          },
+        },
+        grep = {
+          actions = {
+            ["ctrl-r"] = { fzf_lua().actions.grep_lgrep },
+            ["ctrl-g"] = { fzf_lua().actions.toggle_ignore },
+          },
+        },
+        files = {
+          actions = {
+            ["ctrl-e"] = {
+              function(selected, opts)
+                local files = {}
+                for _, file in ipairs(selected) do
+                  local entry = fzf_lua().path.entry_to_file(file)
+                  table.insert(files, entry.stripped)
+                end
+
+                if #files == 0 then
+                  fzf_lua().files({
+                    fd_opts = opts.fd_opts,
+                    prompt = opts.prompt,
+                  })
+                else
+                  fzf_lua().live_grep({
+                    search_paths = files,
+                    prompt = "*Rg(" .. table.concat(files, ", ") .. ") >",
+                  })
+                end
+              end,
+            },
+            ["ctrl-r"] = {
+              function(_, opts)
+                fzf_lua().files({
+                  cwd = vim.fn.fnamemodify(vim.fs.normalize(opts.cwd or vim.uv.cwd()), ":h"),
+                  fd_opts = opts.fd_opts,
+                  prompt = opts.prompt,
+                })
+              end,
+            },
+            ["ctrl-a"] = {
+              function(_, opts)
+                fzf_lua().files({
+                  cwd = vim.uv.cwd(),
+                  fd_opts = opts.fd_opts,
+                  prompt = opts.prompt,
+                })
+              end,
+            },
+          },
+        },
+      })
+    end,
   },
 }
