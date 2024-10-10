c386bfd1f3877b9b56c78736b79b6022cb468bea
Alankrit Joshi <alankritjoshi@gmail.com>
2024-10-10 13:25:14 -0700

    Update .config/aerospace/aerospace.toml Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/nvim-cmp.lua

--- private_dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ private_dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -4,14 +4,13 @@ return {
     event = { "InsertEnter", "CmdLineEnter" },
     dependencies = {
       "hrsh7th/cmp-cmdline",
-      "supermaven-inc/supermaven-nvim",
     },
     opts = function(_, opts)
-      table.insert(opts.sources, 1, {
-        name = "supermaven",
-        group_index = 1,
-        priority = 100,
-      })
+      -- table.insert(opts.sources, 1, {
+      --   name = "supermaven",
+      --   group_index = 1,
+      --   priority = 100,
+      -- })
 
       local cmp = require("cmp")
       -- `:` cmdline setup.
