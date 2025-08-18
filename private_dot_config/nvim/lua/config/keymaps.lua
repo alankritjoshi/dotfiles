c3d171841a23767b3b7712c5277d33cc27b5f8f5
Alankrit Joshi <alankritjoshi@gmail.com>
2025-08-17 22:13:41 -0700

    Update .config/nvim/lua/config/keymaps.lua

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,17 @@ vim.keymap.del("n", "<C-H>")
 vim.keymap.del("n", "<C-J>")
 vim.keymap.del("n", "<C-K>")
 vim.keymap.del("n", "<C-L>")
+
+-- Yank relative file path
+vim.keymap.set("n", "<leader>yr", function()
+  local relative_path = vim.fn.expand("%:.")
+  vim.fn.setreg("+", relative_path)
+  print("Yanked: " .. relative_path)
+end, { desc = "Yank Relative File Path" })
+
+-- You might also want the absolute path
+vim.keymap.set("n", "<leader>ya", function()
+  local absolute_path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", absolute_path)
+  print("Yanked: " .. absolute_path)
+end, { desc = "Yank Absolute File Path" })
