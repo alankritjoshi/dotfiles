8d9cb5428a6aa98944f7eb9e6e86c99971965148
Alankrit Joshi <alankritjoshi@gmail.com>
2025-12-20 23:52:35 -0800

    Neotest for ruby

--- private_dot_config/nvim/lua/plugins/neotest.lua
+++ private_dot_config/nvim/lua/plugins/neotest.lua
@@ -4,12 +4,16 @@ return {
     optional = true,
     dependencies = {
       "nvim-neotest/neotest-python",
+      "zidhuss/neotest-minitest",
+      "olimorris/neotest-rspec",
     },
     opts = {
       adapters = {
         ["neotest-python"] = {
           args = { "-vv", "-s" },
         },
+        ["neotest-minitest"] = {},
+        ["neotest-rspec"] = {},
       },
     },
   },
