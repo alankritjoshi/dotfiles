c8a4b281960ef8bb01a1c08a1d19f75044e7696e
Alankrit Joshi <alankritjoshi@gmail.com>
2025-12-21 02:03:44 -0800

    Shadowenv.vim only for work

--- private_dot_config/nvim/lua/plugins/ruby-shadowenv.lua
+++ private_dot_config/nvim/lua/plugins/ruby-shadowenv.lua
@@ -1,11 +1,10 @@
 return {
-  -- Shadowenv.vim plugin for directory-based environment switching
   {
     "Shopify/shadowenv.vim",
-    priority = 100, -- Load early
-    lazy = false,   -- Load immediately
+    priority = 100,
+    lazy = false,
+    cond = vim.fn.hostname() == "vanik",
     config = function()
-      -- Hook shadowenv on directory changes
       vim.api.nvim_create_autocmd({"DirChanged", "VimEnter"}, {
         callback = function()
           vim.cmd("silent! ShadowenvHook")
